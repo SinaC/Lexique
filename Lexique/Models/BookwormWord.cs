6c3040ef11acce32bf2692baf5609b4464d4ffa6
SinaC <heymbeeck.joel@gmail.com>
2020-03-27 23:42:19 +0100

    Bookworm word added

---
+++ Lexique/Models/BookwormWord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexique.Models
+{
+    public class BookwormWord : WordBase
+    {
+        public char[] ValuableLetters { get; }
+
+        public BookwormWord(string word, char[] valuableLetters) : base(word)
+        {
+            ValuableLetters = valuableLetters;
+        }
+
+        protected override int ComputeValue(string s)
+        {
+            // Longer word means higher value and letter between parenthese rewards more point
+            int value = s.Length;
+            foreach (char valuableLetter in ValuableLetters)
+                if (s.Contains(valuableLetter))
+                    value += 10;
+            return value;
+        }
+    }
+}
